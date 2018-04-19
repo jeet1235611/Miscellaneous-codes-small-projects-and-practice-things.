382bab32131f98c6f9c0deeb8001d01ec96359ff
acoustic_jeet <js.jitendra11@gmail.com>
2018-04-20 03:15:48 +0530

    Add files via upload
    This code supposed to work but isn't really working.
    Check it out.

--- toggle.js
+++ toggle.js
@@ -1,24 +1,27 @@
 var button = document.querySelector("button");
-var isPurple = false;
+//var isPurple = false;
 //Callback function.
 // document.querySelector("body");
 // document.getElementsByTagName("body")[0]
 //Is code ka shortcut h neeche wala code
 //document.body.style.bakground = "prugle";
 
-button.addEventListener("click", function(){
-	// if white make it purple
-	//else make make it white.
-	//so will create a boolean variable is purple - false
+// button.addEventListener("click", function(){
+// 	// if white make it purple
+// 	//else make make it white.
+// 	//so will create a boolean variable is purple - false
 
-	if(isPurple){
-		document.body.style.background = "white";
-		//isPurple = false; //false boolean hona na ki string.
-	}
-	else{
-		document.body.style.background = "purple";
-		//isPurple = true;
-	}
-	isPurple = !isPurple;
-	// document.body.style.background = "purple";
+// 	if(isPurple){
+// 		document.body.style.background = "white";
+// 		//isPurple = false; //false boolean hona na ki string.
+// 	}
+// 	else{
+// 		document.body.style.background = "purple";
+// 		//isPurple = true;
+// 	}
+// 	isPurple = !isPurple;
+// 	// document.body.style.background = "purple";
+
+button.addEventListener("click", function(){
+	document.body.classList.toggle.("purple");
 });
